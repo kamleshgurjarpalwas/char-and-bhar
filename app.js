a2ed50bdde5be17e4d6a12c907a7d4118dd10093
kamleshgurjarpalwas <155581058+kamleshgurjarpalwas@users.noreply.github.com>
2024-03-30 15:26:42 +0530

    disabled remaining boxed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//following code for given 9 goties
 const containerForGreenGoti = document.querySelector(".forGreenStorage");
 const containerForRedGoti = document.querySelector(".forRedStorage");
 for (let i = 0; i < 9; i++) {
@@ -11,6 +12,12 @@ for (let i = 0; i < 9; i++) {
   containerForGreenGoti.appendChild(greenGoti);
 }
 
+//following code given for colering of gotis
+const fRedGivGot = document.querySelectorAll(".elementForRedStorage"); // mean:-for red given goties
+const fGreenGivGot = document.querySelectorAll(".elementForGreenStorage"); //mean:-for green given goties
+let rI = 8; //iterator for red goties
+let gI = 8; //iterators for green goties
+let count = 0;
 const gotiStands = document.querySelectorAll(".goti-stand");
 let turn = true;
 gotiStands.forEach((gs, index) => {
@@ -18,12 +25,36 @@ gotiStands.forEach((gs, index) => {
     if (!this.clicked) {
       if (turn === true) {
         gs.style.background = "#05f545";
+        gs.style.pointerEvents = "none";
+        deleteBox(turn);
+        count++;
+        if (count === 18) boxDisabl();
         turn = false;
       } else {
         gs.style.background = "red";
+        gs.style.pointerEvents = "none";
+        deleteBox(turn);
+        count++;
+        if (count === 18) boxDisabl();
         turn = true;
       }
-      this.clicked = true;
     }
   });
 });
+
+const boxDisabl = () => {
+  for (let butt of gotiStands) {
+    butt.disabled = true;
+    butt.pointerEvents = "none";
+  }
+};
+
+function deleteBox(turn1) {
+  if (turn1) {
+    fGreenGivGot[gI].parentNode.removeChild(fGreenGivGot[gI]);
+    gI--;
+  } else {
+    fRedGivGot[rI].parentNode.removeChild(fRedGivGot[rI]);
+    rI--;
+  }
+}
